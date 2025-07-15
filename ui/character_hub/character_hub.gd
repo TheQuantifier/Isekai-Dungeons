cd3b56e8fef1584780ccffd2a131594634f90524
John Hand <johnhand@Johns-Macbook.local>
2025-07-15 13:31:00 -0400

    Added character customization scene and model rotation with adjustable offsets

--- ui/character_hub/character_hub.gd
+++ ui/character_hub/character_hub.gd
@@ -10,7 +10,7 @@ func _on_play_pressed():
 
 func _on_edit_character_pressed():
 	# Implement this when character editing is ready
-	print("Edit Character pressed")
+	game_manager.change_scene("res://scenes/character_customization/character_customization.tscn")
 
 func _on_view_stats_pressed():
 	game_manager.change_scene("res://ui/stats_view/stats_view.tscn")
